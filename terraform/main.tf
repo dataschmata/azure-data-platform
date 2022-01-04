8280afbce30c15d4ddc2cd8243be9997e4919518
rocksde <dennis.rossberg@gmail.com>
2022-01-04 16:39:47 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,20 @@
+# Azure Provider source and version being used
 terraform {
- backend "azurerm" {
-  resource_group_name  = "tamopstfstates"
-  storage_account_name = "tfstatedevops"
-  container_name       = "terraformgithubexample"
-  key                  = "terraformgithubexample.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  backend "azurerm" {
+   resource_group_name  = "rsg-terraform-dev-westeu-001"
+   storage_account_name = "statfsdevwesteu001"
+   container_name       = "tfstate"
+   key                  = "dasdev.tfstate"
  }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
- version = "~>2.0"
- features {}
+  features {}
 }
