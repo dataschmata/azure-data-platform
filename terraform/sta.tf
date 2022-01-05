4475e6d63d43dec85800a92304978c5c8aad495c
rocksde <dennis.rossberg@gmail.com>
2022-01-05 12:40:13 +0100

    Update sta.tf

--- terraform/sta.tf
+++ terraform/sta.tf
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "sta001" {
 }
 
 resource "azurerm_role_assignment" "role001" {
-  scope                = azurerm_storage_account.sta001.id
+  scope                = azurerm_resource_group.rsg001.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azurerm_client_config.cfg.object_id
 }
