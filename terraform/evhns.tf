fc313bf101f22b137dc35ab4c4585b68d2aa4346
rocksde <dennis.rossberg@gmail.com>
2022-01-05 15:16:44 +0100

    Update evhns.tf

--- terraform/evhns.tf
+++ terraform/evhns.tf
@@ -1,10 +1,9 @@
 resource "azurerm_eventhub_namespace" "evhns001" {
-  name                = "evhns-das-dev-westeu-001"
-  location            = azurerm_resource_group.rsg001.location
-  resource_group_name = azurerm_resource_group.rsg001.name
-  sku                 = "Standard"
-  capacity            = 1
-  
+  name                 = "evhns-das-dev-westeu-001"
+  location             = azurerm_resource_group.rsg001.location
+  resource_group_name  = azurerm_resource_group.rsg001.name
+  sku                  = "Standard"
+  capacity             = 1
   virtual_network_rule = {
     subnet_id = azurerm_subnet.snt001.id
   }
