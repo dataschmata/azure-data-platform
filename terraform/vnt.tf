9586168164af2cfbc8acd782291fab3ffb17d4bc
rocksde <dennis.rossberg@gmail.com>
2022-01-05 11:23:42 +0100

    Update vnt.tf

--- terraform/vnt.tf
+++ terraform/vnt.tf
@@ -18,7 +18,7 @@ resource "azurerm_network_security_rule" "nsgsr001" {
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "Tcp"
+  protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
   source_address_prefix       = chomp(data.http.myip.body)
