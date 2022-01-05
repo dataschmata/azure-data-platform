f15a938ec836a2e042094443c9222043b79e4ecf
rocksde <dennis.rossberg@gmail.com>
2022-01-05 09:43:40 +0100

    Update sta.tf

--- terraform/sta.tf
+++ terraform/sta.tf
@@ -4,6 +4,12 @@ resource "azurerm_storage_account" "sta001" {
   location                 = azurerm_resource_group.rsg001.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.snt001.id]
+  }
 }
 
 resource "azurerm_storage_container" "stc001" {
