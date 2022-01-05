2dc2624ff0ed6762a8c4ca1366918a18b2493f25
rocksde <dennis.rossberg@gmail.com>
2022-01-05 12:33:14 +0100

    Create evhns.tf

---
+++ terraform/evhns.tf
@@ -0,0 +1,15 @@
+resource "azurerm_eventhub_namespace" "evhns001" {
+  name                = "evhns-das-dev-westeu-001"
+  location            = azurerm_resource_group.rsg001.location
+  resource_group_name = azurerm_resource_group.rsg001.name
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+resource "azurerm_eventhub" "evh001" {
+  name                = "evh-das-dev-westeu-001"
+  namespace_name      = azurerm_eventhub_namespace.evhns001.name
+  resource_group_name = azurerm_resource_group.rsg001.name
+  partition_count     = 2
+  message_retention   = 1
+}
