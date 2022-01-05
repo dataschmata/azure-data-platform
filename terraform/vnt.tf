8594c23f9f4551637e6cb36144857d02bff190ab
rocksde <dennis.rossberg@gmail.com>
2022-01-05 10:00:15 +0100

    Update vnt.tf

--- terraform/vnt.tf
+++ terraform/vnt.tf
@@ -6,11 +6,16 @@ resource "azurerm_virtual_network" "vnt001" {
   resource_group_name = azurerm_resource_group.rsg001.name
 }
 
+resource "azurerm_network_security_group" "nsg001" {
+  name                = "nsg-das-dev-westeu-001"
+  location            = azurerm_resource_group.rsg001.location
+  resource_group_name = azurerm_resource_group.rsg001.name
+}
+
 # Create Subnet
 resource "azurerm_subnet" "snt001" {
   name                 = "snt-das-dev-westeu-001"
   resource_group_name  = azurerm_resource_group.rsg001.name
   virtual_network_name = azurerm_virtual_network.vnt001.name
-  address_prefix       = "192.168.0.0/24"
+  address_prefixes       = "192.168.0.0/24"
 }
-
