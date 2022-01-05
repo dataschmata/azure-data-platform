286fa66e85cd048a6fab8a4e54322d3d3a93fc94
rocksde <dennis.rossberg@gmail.com>
2022-01-05 09:48:49 +0100

    Update sta.tf

--- terraform/sta.tf
+++ terraform/sta.tf
@@ -4,7 +4,8 @@ resource "azurerm_storage_account" "sta001" {
   location                 = azurerm_resource_group.rsg001.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
   network_rules {
     default_action             = "Deny"
     ip_rules                   = ["100.0.0.1"]
@@ -18,3 +19,11 @@ resource "azurerm_storage_container" "stc001" {
   container_access_type = "private"
 }
 
+resource "azurerm_storage_data_lake_gen2_filesystem" "stf001" {
+  name               = "raw"
+  storage_account_id = azurerm_storage_account.sta001.id
+
+#   properties = {
+#     hello = "aGVsbG8="
+#   }
+}
