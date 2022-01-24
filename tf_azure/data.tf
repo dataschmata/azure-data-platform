ea524504285359c1cb827c2a62b30d8fe052e1c5
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-24 15:09:39 +0100

    change

--- tf_azure/data.tf
+++ tf_azure/data.tf
@@ -4,3 +4,9 @@ data "http" "myip" {
 
 data "azurerm_client_config" "cfg" {
 }
+
+# data "databricks_current_user" "me" {}
+# data "databricks_spark_version" "latest" {}
+# data "databricks_node_type" "smallest" {
+#   local_disk = true
+# }
--- tf_azure/vnt.tf
+++ tf_azure/vnt.tf
@@ -36,19 +36,19 @@ resource "azurerm_subnet" "snt001" {
   service_endpoints    = ["Microsoft.Storage", "Microsoft.EventHub"]
 }
 
-resource "azurerm_subnet" "snt002" {
-  name                 = "snt-dbwpvt-dev-westeu-002"
-  resource_group_name  = azurerm_resource_group.rsg001.name
-  virtual_network_name = azurerm_virtual_network.vnt001.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_subnet" "snt002" {
+#   name                 = "snt-dbwpvt-dev-westeu-002"
+#   resource_group_name  = azurerm_resource_group.rsg001.name
+#   virtual_network_name = azurerm_virtual_network.vnt001.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
 
-resource "azurerm_subnet" "snt003" {
-  name                 = "snt-dbwpub-dev-westeu-003"
-  resource_group_name  = azurerm_resource_group.rsg001.name
-  virtual_network_name = azurerm_virtual_network.vnt001.name
-  address_prefixes     = ["10.0.3.0/24"]
-}
+# resource "azurerm_subnet" "snt003" {
+#   name                 = "snt-dbwpub-dev-westeu-003"
+#   resource_group_name  = azurerm_resource_group.rsg001.name
+#   virtual_network_name = azurerm_virtual_network.vnt001.name
+#   address_prefixes     = ["10.0.3.0/24"]
+# }
 
 resource "azurerm_subnet_network_security_group_association" "nsga001" {
   subnet_id                 = azurerm_subnet.snt001.id
