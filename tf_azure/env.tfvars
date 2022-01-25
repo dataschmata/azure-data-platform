03349eb767ec313aae8317332107cb34f86f468b
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-25 14:33:04 +0100

    fix tags tfvars

--- tf_azure/env.tfvars
+++ tf_azure/env.tfvars
@@ -3,18 +3,14 @@ subscription_id   = "9aaf76a1-3252-4daa-b293-faac15046878"
 
 environment       = "dev"
 workload          = "das"
+scope             = "data_platform"
+costcenter        = "99999999"
+customeremail     = "customer.email@nomail.tst"
+owner             = "Owner Name"
+owneremail        = "owner.email@nomail.tst"
+scope             = "data_platform"
 
 region = {
     "short"       = "weu1"
     "location"    = "westeurope"
   }
-
-tags = {
-    costcenter    = "99999999"
-    customeremail = "customer.email@nomail.tst"
-    owner         = "Owner Name"
-    owneremail    = "owner.email@nomail.tst"
-    scope         = "data_platform"
-    tier          = $workload
-    environment   = $environment
-    }
--- tf_azure/var.tf
+++ tf_azure/var.tf
@@ -19,10 +19,32 @@ variable "region" {
   }
 }
 
-variable "resource_tags" {
-  description = "Common tags to be used for all resources."
+variable "customer" {
+  description = "The main customer (user) of the resource"
   type        = map
-  default     = { }
+  default     = {
+    "name"    = "Customer Name"
+    "email"   = "customer.name@email.tst"
+  }
 }
 
+variable "owner" {
+  description = "The owne of the deployed resources"
+  type        = map
+  default     = {
+    "name"    = "Owner Name"
+    "email"   = "owner.name@email.tst"
+  }
+}
 
+variable "costcenter" {
+  type = string
+  description = "costcenter"
+  default     = "99999999"
+}
+
+variable "scope" {
+  type = string
+  description = "scope"
+  default     = "data_platform"
+}
