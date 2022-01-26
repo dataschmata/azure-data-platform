142bf1aadd836263c487ade0d01083025eb43432
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-26 22:37:57 +0100

    change plan only yml

--- tf_azure/stc.tf
+++ tf_azure/stc.tf
@@ -1,17 +1,17 @@
 # used due to issue https://github.com/hashicorp/terraform-provider-azurerm/issues/2977
 
-resource "azurerm_template_deployment" "sct100" {
+resource "azurerm_resource_group_template_deployment" "sct100" {
   depends_on          = [azurerm_storage_account.sta100]
-  name                = "raw"
+  name                = "ARM_Template_Container"
   resource_group_name = azurerm_resource_group.rsg_main.name
   deployment_mode     = "Incremental"
-  template_body       = file("${path.module}/container.json")
-  parameters = {
-    storage_account_name = azurerm_storage_account.sta100.name
-    container_name       = "raw"
-  }
-  lifecycle {
-  prevent_destroy = true
-  }
-
+  template_content    = file("${path.module}/container.json")
+  parameters_content  = jsonencode({
+    "storage_account_name" = {
+      value = azurerm_storage_account.sta100.name
+    },
+    "container_name" = {
+      value = "raw"
+    }
+  })
 }
