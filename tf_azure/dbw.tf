0439dabfdf702bc8e38388cd80fc3ccc4a0467d8
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-26 17:26:06 +0100

    change plan only yml

--- tf_azure/dbw.tf
+++ tf_azure/dbw.tf
@@ -10,7 +10,7 @@ resource "azurerm_databricks_workspace" "dbw100" {
     virtual_network_id       = azurerm_virtual_network.vnt_main.id
     public_subnet_name       = azurerm_subnet.snt_pub.name
     private_subnet_name      = azurerm_subnet.snt_pvt.name
-    storage_account_name     = azurerm_storage_account.sta110.name
+    storage_account_name     = replace("sta${local.name_conv}110", "-", "")
     storage_account_sku_name = "Standard_LRS"
 
     private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.nga_pvt.id
