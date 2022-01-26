7a5ea95b3e35c2f4289b8aeed2e86cb5cb74ca19
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-26 11:51:33 +0100

    snt chnage for dbw

--- tf_azure/evhns.tf
+++ tf_azure/evhns.tf
@@ -20,7 +20,6 @@ resource "azurerm_eventhub" "evh100" {
   resource_group_name = azurerm_resource_group.rsg_main.name
   partition_count     = 2
   message_retention   = 1
-  tags                = local.tags
 }
 
 resource "azurerm_eventhub_namespace_authorization_rule" "ehr100" {
