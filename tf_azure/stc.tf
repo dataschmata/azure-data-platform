f72bffdd01843cb0e92727d728f86e1a1f9e2338
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-26 22:47:21 +0100

    change plan only yml

--- tf_azure/stc.tf
+++ tf_azure/stc.tf
@@ -7,11 +7,7 @@ resource "azurerm_resource_group_template_deployment" "sct100" {
   deployment_mode     = "Incremental"
   template_content    = file("${path.module}/container.json")
   parameters_content  = jsonencode({
-    "storage_account_name" = {
-      value = azurerm_storage_account.sta100.name
-    },
-    "container_name" = {
-      value = "raw"
-    }
+    "storage_account_name" = {value = azurerm_storage_account.sta100.name},
+    "container_name"       = {value = "raw"}
   })
 }
