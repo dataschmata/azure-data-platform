303d0e86412e35596d8154ce8532ecdc39c2a883
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-27 15:30:18 +0100

    add add groups and members

--- tf_azure/roles.tf
+++ tf_azure/roles.tf
@@ -3,3 +3,15 @@ resource "azurerm_role_assignment" "role100" {
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = data.azurerm_client_config.cfg.object_id
 }
+
+resource "azurerm_role_assignment" "role_adm" {
+  scope                = azurerm_resource_group.rsg_main.id
+  role_definition_name = "Owner"
+  principal_id         = azuread_group.grp_adm.object_id
+}
+
+resource "azurerm_role_assignment" "role_adm_dbw" {
+  scope                = azurerm_resource_group.rsg_main.id
+  role_definition_name = "Owner"
+  principal_id         = azurerm_databricks_workspace.dbw100.object_id
+}
