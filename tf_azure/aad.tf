fff9f3fde9c3737e05bc238e16965b3a72b26415
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-27 14:58:22 +0100

    add add groups and members

--- tf_azure/aad.tf
+++ tf_azure/aad.tf
@@ -18,8 +18,8 @@ resource "azuread_user" "usr_adm_dbw" {
 resource "azuread_group" "grp_adm" {
   for_each         = azuread_user.usr_adm
   display_name     = "az-${local.workload}-admin"
-  # owners           = [data.azuread_client_config.ad_current.object_id]
   security_enabled = true
+  # owners           = [data.azuread_client_config.ad_current.object_id]
 
   members = [
     each.value.object_id,
@@ -30,8 +30,8 @@ resource "azuread_group" "grp_adm" {
 resource "azuread_group" "grp_adm_dbw" {
   for_each         = azuread_user.usr_adm_dbw
   display_name     = "az-${local.workload}-admin_dbw"
-  # owners           = [data.azuread_client_config.ad_current.object_id]
   security_enabled = true
+  # owners           = [data.azuread_client_config.ad_current.object_id]
 
   members = [
     each.value.object_id,
