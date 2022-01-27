bbbe1a5f31355e1925b018472cecc16d780fe3fd
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-27 16:11:54 +0100

    add add groups and members

--- tf_azure/roles.tf
+++ tf_azure/roles.tf
@@ -14,6 +14,6 @@ resource "azurerm_role_assignment" "role_adm" {
 resource "azurerm_role_assignment" "role_adm_dbw" {
   for_each             = azuread_group.grp_adm_dbw
   scope                = azurerm_databricks_workspace.dbw100.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Owner"
   principal_id         = each.value.object_id
 }
