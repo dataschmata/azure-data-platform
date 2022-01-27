1a3463b4b348a7c6695f3bebaa210ed56bd8f5b3
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-27 11:35:22 +0100

    change plan only yml

--- tf_azure/sta.tf
+++ tf_azure/sta.tf
@@ -1,16 +1,20 @@
 resource "azurerm_storage_account" "sta100" {
-  name                     = replace("sta${local.name_conv}100", "-", "")
-  resource_group_name      = azurerm_resource_group.rsg_main.name
-  location                 = var.region["location"]
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  is_hns_enabled           = "true"
-  tags                     = local.tags
+  name                      = replace("sta${local.name_conv}100", "-", "")
+  resource_group_name       = azurerm_resource_group.rsg_main.name
+  location                  = var.region["location"]
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  is_hns_enabled            = true
+  enable_https_traffic_only = true
+  access_tier               = "Hot"
+  allow_blob_public_access  = false
+  tags                      = local.tags
   network_rules {
     default_action             = "Deny"
     ip_rules                   = [chomp(data.http.myip.body)]
     virtual_network_subnet_ids = [azurerm_subnet.snt_main.id]
+    bypass                     = [ "Logging","AzureServices","Metrics" ] 
   }
 }
 
@@ -18,6 +22,25 @@ resource "azurerm_role_assignment" "role100" {
   scope                = azurerm_resource_group.rsg_main.id
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = data.azurerm_client_config.cfg.object_id
+}
+
+resource "azurerm_advanced_threat_protection" "atp100" {
+  target_resource_id = azurerm_storage_account.sta100.id
+  enabled            = true
+}
+
+resource "azurerm_private_endpoint" "pep_sta100" {
+  name                = "pep-snt100-sta100-${local.name_conv}"
+  location            = var.region["location"]
+  resource_group_name = azurerm_resource_group.rsg_main.name
+  subnet_id           = azurerm_subnet.snt_main.id
+  tags                = local.tags
+  private_service_connection {
+    name                           = "pep-snt100-sta100-${local.name_conv}"
+    private_connection_resource_id = azurerm_storage_account.sta100.id
+    subresource_names              = [ "dfs" ]
+    is_manual_connection           = false
+  }
 }
 
 ### known issue https://github.com/hashicorp/terraform-provider-azurerm/issues/2977, usin ARM template as suggested in issue
