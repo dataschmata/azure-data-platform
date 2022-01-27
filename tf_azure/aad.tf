db14d6886901805e3174859809bce2479ceb8c7a
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-27 15:01:02 +0100

    add add groups and members

--- tf_azure/aad.tf
+++ tf_azure/aad.tf
@@ -2,17 +2,17 @@
 resource "azuread_user" "usr_adm" {
   for_each            = toset(var.admin_email)
   display_name        = each.key
-  # owners              = [data.azuread_client_config.ad_current.object_id]
   password            = "This1sA8adPa$$w0rd1!"
   user_principal_name = each.key
+  # owners              = [data.azuread_client_config.ad_current.object_id]
 }
 
 resource "azuread_user" "usr_adm_dbw" {
   for_each            = toset(var.admin_dbw_email)
   display_name        = each.key
-  # owners              = [data.azuread_client_config.ad_current.object_id]
   password            = "This1sA8adPa$$w0rd1!"
   user_principal_name = each.key
+  # owners              = [data.azuread_client_config.ad_current.object_id]
 }
 
 resource "azuread_group" "grp_adm" {
