01764119006b8be45775e2af36f4971bdf922092
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-28 14:05:15 +0100

    add add groups and members

--- tf_azure/main.tf
+++ tf_azure/main.tf
@@ -26,8 +26,6 @@ terraform {
 provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-  client_secret   = var.openDoor
-  client_id       = var.client_id
   features {}
 }
 
