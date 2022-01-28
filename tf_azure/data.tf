6ee1fd9f5fb87f63577ecb6ef31b4ba537c1cee3
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-01-28 14:41:02 +0100

    add add groups and members

--- tf_azure/data.tf
+++ tf_azure/data.tf
@@ -6,17 +6,17 @@ data "azurerm_client_config" "cfg" {}
 
 data "azuread_client_config" "ad_current" {}
 
-# data "databricks_group" "admins" {
-#   display_name = "admins"
-#   depends_on   = [azurerm_databricks_workspace.dbw100]
-# }
+data "databricks_group" "admins" {
+  display_name = "admins"
+  depends_on   = [azurerm_databricks_workspace.dbw100]
+}
 
 data "databricks_node_type" "dbc_smallest" {
   local_disk = true
   depends_on = [azurerm_databricks_workspace.dbw100]
 }
 
-# data "databricks_spark_version" "dbc_latest_lts" {
-#   long_term_support = true
-#   depends_on        = [azurerm_databricks_workspace.dbw100]
-# }
+data "databricks_spark_version" "dbc_latest_lts" {
+  long_term_support = true
+  depends_on        = [azurerm_databricks_workspace.dbw100]
+}
