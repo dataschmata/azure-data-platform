740d454535478da52ca804080700384437e8c8b4
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-02-02 18:33:34 +0100

    sp plus sp secret creation for dbw use fix

--- tf_azure/databricks_access.tf
+++ tf_azure/databricks_access.tf
@@ -16,7 +16,7 @@ resource "databricks_secret" "secret" {
 # users and groups
 resource "databricks_user" "admin_users" {
   for_each   = azuread_group.grp_adm_dbw
-  user_name  = each.key
+  user_name  = each.value.user_principal_name
   depends_on = [azurerm_databricks_workspace.dbw100]
 }
 
