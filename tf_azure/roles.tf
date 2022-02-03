cd525ca4ec27262363da7ffaa871e7b730af3337
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-02-03 16:51:36 +0100

    change add users and key vault role

--- tf_azure/roles.tf
+++ tf_azure/roles.tf
@@ -36,7 +36,7 @@ resource "azurerm_role_assignment" "role_kvt_sec" {
   principal_id         = azuread_group.grp_adm.object_id
 }
 
-resource "azurerm_role_assignment" "role_kvt_sec" {
+resource "azurerm_role_assignment" "role_kvt_sec_sp" {
   scope                = azurerm_key_vault.kvt_main.id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = local.object_id
