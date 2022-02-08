0f015d57295791a923cafc6923b74dcfe3b1f051
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-02-08 10:14:09 +0100

    dbw users setup fix

--- databricks_security.tf
+++ databricks_security.tf
@@ -18,8 +18,8 @@ resource "databricks_secret" "secret" {
 resource "databricks_user" "users" {
   for_each = toset(
     concat(
-      [data.azuread_users.usr_adm_dbw.user_principal_names],
-      [data.azuread_users.usr_usr_dbw.user_principal_names],
+      data.azuread_users.usr_adm_dbw.user_principal_names,
+      data.azuread_users.usr_usr_dbw.user_principal_names,
     )
   )
   user_name = each.key
